8d12ed14e714eafc2340beaf93d516dcc23117dc
Picard Quentin <quentinp27190@hotmail.fr>
2024-07-19 10:31:50 +0200

    Mise en place de l'api freelances sur la page freelance afin de récuperer des données de freelance

--- shiny-agency/src/pages/Freelances/index.jsx
+++ shiny-agency/src/pages/Freelances/index.jsx
@@ -1,12 +1,13 @@
-import {freelanceProfiles} from '../../data/freelanceProfiles/freelanceProfiles';
-import Card from "../../componants/Card";
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Card from "../../componants/Card";
+import { Loader } from '../../utils/style/Atoms';
 
 const CardsContainer = styled.div`
     display: flex;
-    width:100%;
-    margin-top : 3%;
-    height : 65vh;
+    width: 100%;
+    margin-top: 3%;
+    height: 65vh;
     flex-wrap: wrap;
     justify-content: space-between;
     align-content: space-around;
@@ -14,42 +15,80 @@ const CardsContainer = styled.div`
 
 const MainContent = styled.div`
     width: 65%;
-    margin : 64px auto;
+    margin: 64px auto;
 `;
 
 const TitleContent = styled.div`
-    width : 100%;
-    display : flex;
-    flex-direction : column;
-    align-items:center
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
-
+const Loading = styled.div`
+    width:100%;
+    backgroundcolor : red
+`;
 const Title = styled.h2`
-    font-size : 30px;
+    font-size: 30px;
     font-family: 'Trebuchet MS', Helvetica, sans-serif;
 `;
 
+function Profils() {
+    const [freelances, setFreelances] = useState([]);
+    const [isDataLoading, setDataLoading] = useState(false);
+    const [error, setError] = useState(false);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            setDataLoading(true);
+            try {
+                const response = await fetch('http://localhost:8000/freelances');
+                const data = await response.json();
+                console.log('API response:', data.freelancersList); 
+                setFreelances(data.freelancersList); // Assurez-vous que 'data' est directement un tableau d'objets
+            } catch (err) {
+                console.log('===== error =====', err);
+                setError(true);
+            } finally {
+                setDataLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
 
-function Profils () {
+    if (error) {
+        return <div>Error: Une erreur est survenue lors du chargement des données.</div>;
+    }
+
     return (
         <MainContent>
             <TitleContent>
                 <Title>Trouvez votre prestataire</Title>
                 <p>Chez Shiny nous réunissons les meilleurs profils pour vous.</p>
             </TitleContent>
-            <CardsContainer>
-                {freelanceProfiles.map((profile, index) => (
-                    <Card
-                        key={`${profile.name}-${index}`}
-                        label={profile.jobTitle}
-                        name={profile.name}
-                    />
-                ))}
-            </CardsContainer>
+            {isDataLoading ? (
+                <Loading>
+                    <Loader />
+                </Loading>
+                
+            ) : (
+                <CardsContainer>
+                    {freelances.length > 0 ? (
+                        freelances.map((profile) => (
+                            <Card
+                                key={profile.id} // Assurez-vous que chaque 'profile' a une propriété 'id'
+                                label={profile.job}
+                                name={profile.name}
+                                picture={profile.picture} // Ajouter des informations supplémentaires si nécessaire
+                            />
+                        ))
+                    ) : (
+                        <div>Aucun profil disponible</div>
+                    )}
+                </CardsContainer>
+            )}
         </MainContent>
-    )
+    );
 }
-
 
 export default Profils;
--- shiny-agency/src/pages/Survey.jsx
+++ shiny-agency/src/pages/Survey.jsx
@@ -1,8 +1,8 @@
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { colors } from '../utils/style/colors'
 import { Loader } from '../utils/style/Atoms'
 
@@ -10,16 +10,16 @@ const SurveyContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const QuestionTitle = styled.h2`
   text-decoration: underline;
   text-decoration-color: ${colors.primary};
-`
+`;
 
 const QuestionContent = styled.span`
   margin: 30px;
-`
+`;
 
 const LinkWrapper = styled.div`
   padding-top: 30px;
@@ -38,6 +38,7 @@ function Survey() {
   const nextQuestionNumber = questionNumberInt + 1
   const [surveyData, setSurveyData] = useState({})
   const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   // Cette syntaxe permet aussi bien de faire des calls API.
   // Mais pour utiliser await dans une fonction, il faut que celle-ci soit async (pour asynchrone).
@@ -58,14 +59,32 @@ function Survey() {
 
   useEffect(() => {
     // fetchData()
-    setDataLoading(true)
-    fetch(`http://localhost:8000/survey`).then((response) =>
-      response.json().then(({ surveyData }) => {
+    // setDataLoading(true)
+    // fetch(`http://localhost:8000/survey`).then((response) =>
+    //   response.json().then(({ surveyData }) => {
+    //     setSurveyData(surveyData)
+    //     setDataLoading(false)
+    //   })
+    // )
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json()
         setSurveyData(surveyData)
+      } catch (err) {
+        console.log('===== error =====', err)
+        setError(true)
+      } finally {
         setDataLoading(false)
-      })
-    )
+      }
+    }
+    fetchSurvey()
   }, [])
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
 
   return (
     <SurveyContainer>
